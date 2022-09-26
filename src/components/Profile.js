fdc855b4bca109fd62c7d83aec0959913aed0794
Dimitar Gluharski <dimitargluharski@gmail.com>
2022-09-26 08:56:59 +0300

    add conditional rendering and css module

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,14 @@
-import '../App.css';
+import styles from './Profile.module.css';
 
 const Profile = ({ matchesCount }) => {
-	console.log(matchesCount);
 	return (
-		<div>
+		<div className={matchesCount.length >= 10
+			? styles['hot']
+			: styles['normal']
+		}>
 			{matchesCount.length
 				? `${matchesCount.length}`
-				: 'No favorite matches'
+				: ''
 			}
 		</div>
 	)
